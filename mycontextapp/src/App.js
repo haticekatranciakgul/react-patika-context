7ede03f2a2a5ddb9dd2832191ac79fb4dbbf344b
Hatice KATRANCI AKGÜL <76768140+haticekatranciakgul@users.noreply.github.com>
2024-04-17 00:00:04 +0300

    themecontext

---
+++ mycontextapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Button from './components/Button';
+import ThemeContext from './context/ThemeContext';
+
+function App() {
+  return (
+ 
+     <ThemeContext.Provider value="dark">
+      <Button></Button>
+
+
+     </ThemeContext.Provider>
+
+  );
+}
+
+export default App;
